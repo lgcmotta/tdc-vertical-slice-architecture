aec90d3b6b9f698a06916fcf9278a3857429fa44
lgcmotta <luiz.motta@warren.com.br>
2023-05-19 18:09:44 -0300

    feat: remove program class and prepare to use minimal APIs

--- src/BankingApp.Api/Program.cs
+++ src/BankingApp.Api/Program.cs
@@ -1,44 +1,44 @@
 using Autofac.Extensions.DependencyInjection;
+using BankingApp.Api;
 using BankingApp.Application.Policies;
 using BankingApp.Infrastructure.EntityFramework;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
-using System.Threading.Tasks;
 
-namespace BankingApp.Api;
+// TODO: migrate this app to use minimal APIs
+// var builder = WebApplication.CreateBuilder();
+//
+// var app = builder.Build();
+//
+// await app.RunAsync();
 
-public class Program
+try
 {
-    public static async Task Main(string[] args)
-    {
-        try
-        {
-            var host = CreateHostBuilder(args).Build();
+    var host = CreateHostBuilder(args).Build();
 
-            var policy = PolicyFactory.CreateAsyncRetryPolicy(Log.Logger);
+    var policy = PolicyFactory.CreateAsyncRetryPolicy(Log.Logger);
 
-            await policy.ExecuteAsync(async () => { await host.Services.MigrateDbContextAsync(); });
+    await policy.ExecuteAsync(async () => { await host.Services.MigrateDbContextAsync(); });
 
-            await host.RunAsync();
-        }
-        catch (Exception exception)
-        {
-            Log.Logger.Fatal(exception, "Host terminated unexpectedly.");
-        }
-        finally
-        {
-            Log.CloseAndFlush();
-        }
-    }
-
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-            .UseSerilog()
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+    await host.RunAsync();
 }
+catch (Exception exception)
+{
+    Log.Logger.Fatal(exception, "Host terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+        .UseSerilog()
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
+        });
