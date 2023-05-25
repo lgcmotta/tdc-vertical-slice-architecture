aeb02c4ddda926072e959e08366666f4f2ae6f84
lgcmotta <luiz.motta@warren.com.br>
2023-05-25 09:54:22 -0300

    feat: add account value objects

---
+++ src/BankingApp.Accounts.Domain/ValueObjects/TransactionType.cs
@@ -0,0 +1,15 @@
+﻿using BankingApp.Domain.Core;
+
+namespace BankingApp.Accounts.Domain.ValueObjects;
+
+public sealed class TransactionType : ValueObject<int, string>
+{
+    private TransactionType(int key, string value) : base(key, value)
+    { }
+
+    public static TransactionType Deposit => new(0, nameof(Deposit));
+    public static TransactionType Withdraw => new(1, nameof(Withdraw));
+    public static TransactionType Payment => new(2, nameof(Payment));
+    public static TransactionType Transfer => new(3, nameof(Transfer));
+    public static TransactionType Earnings => new(4, nameof(Earnings));
+}
