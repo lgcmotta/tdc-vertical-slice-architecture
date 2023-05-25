651f67f8183cb2d8dc2782b18ea65098d751ad6c
lgcmotta <luiz.motta@warren.com.br>
2023-05-25 10:21:47 -0300

    refactor: move AggregateRoot to another file

---
+++ src/BankingApp.Domain.Core/AggregateRoot.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System.Collections.ObjectModel;
+
+namespace BankingApp.Domain.Core;
+
+public abstract class AggregateRoot<TId> : IEntity<TId>, IAggregateRoot
+{
+    private readonly List<INotification> _domainEvents = new();
+
+    public TId Id { get; protected set;  } = default!;
+
+    public IReadOnlyCollection<INotification> DomainEvents => new ReadOnlyCollection<INotification>(_domainEvents);
+
+    public void AddDomainEvent(INotification domainEvent) => _domainEvents.Add(domainEvent);
+
+    public void RemoveDomainEvent(INotification domainEvent) => _domainEvents.Remove(domainEvent);
+
+    public void ClearDomainEvents() => _domainEvents.Clear();
+}
--- src/BankingApp.Domain.Core/IAggregateRoot.cs
+++ src/BankingApp.Domain.Core/IAggregateRoot.cs
@@ -1,5 +1,4 @@
 ﻿using MediatR;
-using System.Collections.ObjectModel;
 
 namespace BankingApp.Domain.Core;
 
@@ -12,19 +11,4 @@ public interface IAggregateRoot
     public void RemoveDomainEvent(INotification domainEvent);
 
     public void ClearDomainEvents();
-}
-
-public abstract class AggregateRoot<TId> : IEntity<TId>, IAggregateRoot
-{
-    private readonly List<INotification> _domainEvents = new();
-
-    public TId Id { get; protected set;  } = default!;
-
-    public IReadOnlyCollection<INotification> DomainEvents => new ReadOnlyCollection<INotification>(_domainEvents);
-
-    public void AddDomainEvent(INotification domainEvent) => _domainEvents.Add(domainEvent);
-
-    public void RemoveDomainEvent(INotification domainEvent) => _domainEvents.Remove(domainEvent);
-
-    public void ClearDomainEvents() => _domainEvents.Clear();
 }
