df2f5722e0982cb0a6a794d2826884ec9eb0ca1e
lgcmotta <luiz.motta@warren.com.br>
2023-05-25 18:36:57 -0300

    fix: remove duplicated global using statements

--- tests/BankingApp.Transactions.UnitTests/Usings.cs
+++ tests/BankingApp.Transactions.UnitTests/Usings.cs
@@ -2,8 +2,6 @@ global using BankingApp.Transactions.Domain.Exceptions;
 global using BankingApp.Transactions.Domain.ValueObjects;
 global using BankingApp.Transactions.Domain;
 global using Bogus.Extensions.Brazil;
-global using Bogus.Extensions.Brazil;
-global using Bogus;
 global using Bogus;
 global using FluentAssertions;
 global using System.Collections;
