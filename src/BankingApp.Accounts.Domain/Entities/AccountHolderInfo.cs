d673fc13424d1c168502ee67c4dd9fa0d2aa283c
lgcmotta <luiz.motta@warren.com.br>
2023-05-25 09:55:16 -0300

    feat: create entities AccountHolderInfo and Transaction

---
+++ src/BankingApp.Accounts.Domain/Entities/AccountHolderInfo.cs
@@ -0,0 +1,37 @@
+﻿using BankingApp.Domain.Core;
+
+namespace BankingApp.Accounts.Domain.Entities;
+
+public sealed class AccountHolderInfo : IModifiable
+{
+    private AccountHolderInfo()
+    { }
+
+    public AccountHolderInfo(string name, string document, string email, string phoneNumber) : this()
+    {
+        if (string.IsNullOrWhiteSpace(name)) throw new ArgumentNullException(nameof(name));
+        if (string.IsNullOrWhiteSpace(document)) throw new ArgumentNullException(nameof(document));
+        if (string.IsNullOrWhiteSpace(email)) throw new ArgumentNullException(nameof(email));
+        if (string.IsNullOrWhiteSpace(phoneNumber)) throw new ArgumentNullException(nameof(phoneNumber));
+
+        Name = name;
+        Document = document;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public string Name { get; private set; }
+
+    public string Document { get; private set; }
+
+    public string Email { get; private set; }
+
+    public string PhoneNumber { get; private set; }
+
+    public DateTime ModifiedAt { get; private set; }
+
+    public void LastModifiedAt(DateTime modifiedAt)
+    {
+        ModifiedAt = modifiedAt;
+    }
+}
---
+++ src/BankingApp.Accounts.Domain/Entities/Transaction.cs
@@ -0,0 +1,33 @@
+﻿using BankingApp.Accounts.Domain.ValueObjects;
+using BankingApp.Domain.Core;
+
+namespace BankingApp.Accounts.Domain.Entities;
+
+public sealed class Transaction : IEntity<Guid>
+{
+    private readonly Money _value;
+    private readonly Money _balanceSnapShot;
+
+    private Transaction()
+    { }
+
+    public Transaction(Money value, Money balanceSnapShot, TransactionType type, DateTime occurence) : this()
+    {
+        _value = value;
+        _balanceSnapShot = balanceSnapShot;
+        Type = type;
+        Occurence = occurence;
+    }
+
+    public Guid Id { get; private set; }
+    public TransactionType Type { get; private set; }
+    public DateTime Occurence { get; private set; }
+
+    internal Money Value => IsCreditTransaction()
+        ? _value
+        : _value.Negative();
+
+    private bool IsCreditTransaction() => Type == TransactionType.Deposit || Type == TransactionType.Earnings;
+
+    internal Money GetBalanceBeforeTransaction() => _balanceSnapShot;
+}
