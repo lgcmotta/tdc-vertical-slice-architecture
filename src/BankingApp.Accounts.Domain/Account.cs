dd56f550312b7ff1f4f38c4f47b98dec3ff72554
lgcmotta <luiz.motta@warren.com.br>
2023-05-25 09:55:29 -0300

    feat: add Account aggregate

---
+++ src/BankingApp.Accounts.Domain/Account.cs
@@ -0,0 +1,146 @@
+﻿using BankingApp.Accounts.Domain.Entities;
+using BankingApp.Accounts.Domain.Events;
+using BankingApp.Accounts.Domain.Exceptions;
+using BankingApp.Accounts.Domain.ValueObjects;
+using BankingApp.Domain.Core;
+using System.Collections.ObjectModel;
+
+namespace BankingApp.Accounts.Domain;
+
+public sealed class Account : AggregateRoot<Guid>, IModifiable
+{
+    private List<Transaction> _transactions = new();
+    private DateTime _modifiedAt;
+
+    private Account()
+    { }
+
+    public Account(
+        string name,
+        string document,
+        string email,
+        string phoneNumber,
+        string token,
+        Currency currency) : this()
+    {
+        if (string.IsNullOrWhiteSpace(token)) throw new ArgumentNullException(nameof(token));
+
+        Token = token;
+        Currency = currency ?? throw new ArgumentNullException(nameof(currency));
+        Balance = Money.Zero;
+        HolderInfo = new AccountHolderInfo(name, document, email, phoneNumber);
+    }
+
+    public string Token { get; private set; }
+    public Money Balance { get; private set; }
+    public Currency Currency { get; private set; }
+    public AccountHolderInfo HolderInfo { get; private set; }
+
+    public IEnumerable<Transaction> Transactions => new ReadOnlyCollection<Transaction>(_transactions);
+
+    public string GetFormattedCurrentBalance()
+    {
+        return Balance.Format(Currency);
+    }
+
+    public decimal GetCurrentBalance()
+    {
+        return Balance.Value;
+    }
+
+    public IEnumerable<Transaction> GetBalanceStatementByPeriod(DateTime start, DateTime end)
+    {
+        start = StartOfDay(start);
+        end = EndOfDay(end);
+
+        var transactions = _transactions.Where(transaction => transaction.Occurence >= start && transaction.Occurence <= end);
+
+        return transactions;
+    }
+
+    public void Deposit(Money amount, Currency currency, DateTime transactionDateTime)
+    {
+        if (amount <= Money.Zero)
+        {
+            throw new InvalidTransactionValueException("Deposit amount must be greater than zero.");
+        }
+
+        var usd = amount / currency.DollarRate;
+
+        _transactions.Add(new Transaction(usd, Balance, TransactionType.Deposit, transactionDateTime));
+
+        Balance = new Money(Balance + usd);
+
+        AddBalanceChangedDomainEvent(transactionDateTime);
+    }
+
+    public void Withdraw(Money amount, DateTime transactionDateTime)
+    {
+        if (amount <= Money.Zero)
+        {
+            throw new InvalidTransactionValueException("Withdraw amount must be greater than zero.");
+        }
+
+        var usd = amount * Currency.DollarRate;
+
+        _transactions.Add(new Transaction(usd, Balance, TransactionType.Withdraw, transactionDateTime));
+
+        Balance = new Money(Balance - usd);
+
+        AddBalanceChangedDomainEvent(transactionDateTime);
+    }
+
+    public void Transfer(Money amount, DateTime transactionDateTime)
+    {
+        if (amount <= Money.Zero)
+        {
+            throw new InvalidTransactionValueException("Transfer amount must be greater than zero.");
+        }
+
+        var usd = amount * Currency.DollarRate;
+
+        _transactions.Add(new Transaction(usd, Balance, TransactionType.Transfer, transactionDateTime));
+
+        Balance = new Money(Balance - usd);
+
+        AddBalanceChangedDomainEvent(transactionDateTime);
+    }
+
+    public void ApplyEarnings(Money earnings, DateTime transactionDateTime)
+    {
+        if (earnings <= Money.Zero)
+        {
+            throw new InvalidTransactionValueException("Earnings must be greater than zero.");
+        }
+
+        var usd = earnings * Currency.Dollar.DollarRate;
+
+        _transactions.Add(new Transaction(usd, Balance, TransactionType.Earnings, transactionDateTime));
+
+        Balance = new Money(Balance * usd);
+    }
+
+    public void ChangeCurrency(Currency currency)
+    {
+        if (currency != Currency)
+        {
+            Currency = currency;
+        }
+    }
+
+    private void AddBalanceChangedDomainEvent(DateTime transactionDateTime)
+    {
+        AddDomainEvent(new AccountBalanceChangedDomainEvent(Balance, transactionDateTime));
+    }
+
+    private static DateTime StartOfDay(DateTime dateTime) =>
+        new(dateTime.Year, dateTime.Month, dateTime.Day, 0, 0, 0, 0);
+
+    private static DateTime EndOfDay(DateTime dateTime) =>
+        new(dateTime.Year, dateTime.Month, dateTime.Day, 23, 59, 59, 999);
+
+    public void LastModifiedAt(DateTime modifiedAt)
+    {
+        _modifiedAt = modifiedAt;
+    }
+}
