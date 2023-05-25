e9809d14c1c096a20fcc4c144a06689ec45128c3
lgcmotta <luiz.motta@warren.com.br>
2023-05-25 09:56:38 -0300

    test: unit testing the account aggregate

---
+++ tests/BankingApp.Accounts.UnitTests/Usings.cs
@@ -0,0 +1,5 @@
+global using BankingApp.Accounts.Domain.Exceptions;
+global using BankingApp.Accounts.Domain.ValueObjects;
+global using BankingApp.Accounts.Domain;
+global using FluentAssertions;
+global using Xunit;
