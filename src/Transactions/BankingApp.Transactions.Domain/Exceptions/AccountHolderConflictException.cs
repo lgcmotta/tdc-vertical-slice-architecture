0d6f3d2cd15c3ed56753df46657af504f8ebd6dc
lgcmotta <luiz.motta@warren.com.br>
2023-05-26 19:02:52 -0300

    feat: add new exceptions to transactions domain

---
+++ src/Transactions/BankingApp.Transactions.Domain/Exceptions/AccountHolderConflictException.cs
@@ -0,0 +1,7 @@
+﻿namespace BankingApp.Transactions.Domain.Exceptions;
+
+public class AccountHolderConflictException : Exception
+{
+    public AccountHolderConflictException(string? message) : base(message)
+    { }
+}
