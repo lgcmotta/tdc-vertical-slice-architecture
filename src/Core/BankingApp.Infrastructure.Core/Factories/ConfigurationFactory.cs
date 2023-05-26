58dc67973532da806a69ac601fd648c779bae157
lgcmotta <luiz.motta@warren.com.br>
2023-05-26 01:21:36 -0300

    feat: create infrastructure core csproj

---
+++ src/Core/BankingApp.Infrastructure.Core/Factories/ConfigurationFactory.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace BankingApp.Infrastructure.Core.Factories;
+
+public static class ConfigurationFactory
+{
+    public static IConfiguration CreateConfiguration()
+    {
+        var configurationBuilder = new ConfigurationBuilder();
+
+        configurationBuilder.AddJsonFile("appsettings.json");
+
+        var aspNetCoreEnvironment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        var dotnetCoreEnvironment = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT");
+
+        if (!string.IsNullOrWhiteSpace(aspNetCoreEnvironment))
+        {
+            configurationBuilder.AddJsonFile(
+                path: $"appsettings.{aspNetCoreEnvironment}.json",
+                optional: true,
+                reloadOnChange: true
+            );
+        }
+
+        if (!string.IsNullOrWhiteSpace(dotnetCoreEnvironment))
+        {
+            configurationBuilder.AddJsonFile(
+                path: $"appsettings.{dotnetCoreEnvironment}.json",
+                optional: true,
+                reloadOnChange: true
+            );
+        }
+
+        configurationBuilder.AddEnvironmentVariables();
+
+        return configurationBuilder.Build();
+    }
+}
