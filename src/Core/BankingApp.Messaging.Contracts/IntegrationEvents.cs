126197e3c83ba0d010a415c95cc420abeb510556
lgcmotta <luiz.motta@warren.com.br>
2023-05-26 19:03:25 -0300

    feat: add messaging contracts csproj

---
+++ src/Core/BankingApp.Messaging.Contracts/IntegrationEvents.cs
@@ -0,0 +1,13 @@
+﻿using MassTransit;
+
+// ReSharper disable ClassNeverInstantiated.Global
+namespace BankingApp.Messaging.Contracts;
+
+[MessageUrn("account-created")]
+public record AccountCreatedIntegrationEvent(Guid HolderId, string Name, string Document, string Token, string Currency);
+
+[MessageUrn("account-updated")]
+public record AccountUpdatedIntegrationEvent(Guid HolderId, string? Name, string? Document, string? Token, string? Currency);
+
+[MessageUrn("account-balance-changed")]
+public record AccountBalanceChangedIntegrationEvent(Guid HolderId, decimal Balance, DateTime TransactionTimeStamp);
