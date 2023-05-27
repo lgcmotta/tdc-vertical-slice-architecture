a9d2000c457c989d32cc93d7ffd1f5d61c2946ec
lgcmotta <luiz.motta@warren.com.br>
2023-05-26 23:17:43 -0300

    feat: create earnings slice

---
+++ src/Transactions/BankingApp.Transactions.API/Features/Earnings/ApplyEarningsCommand.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+
+// ReSharper disable ClassNeverInstantiated.Global
+namespace BankingApp.Transactions.API.Features.Earnings;
+
+public record ApplyEarningsCommand(Guid HolderId, decimal Earnings) : IRequest;
