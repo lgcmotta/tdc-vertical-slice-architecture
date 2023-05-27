2f2540a8d758c6806e7d847b1ffb498e084ceb5a
lgcmotta <luiz.motta@warren.com.br>
2023-05-27 18:47:21 -0300

    chore: configure transactions API to use swagger

--- src/Transactions/BankingApp.Transactions.API/Program.cs
+++ src/Transactions/BankingApp.Transactions.API/Program.cs
@@ -1,25 +1,48 @@
+using BankingApp.Application.Core.Behaviors;
 using BankingApp.Infrastructure.Core.Extensions;
+using BankingApp.Transactions.API.Features.Deposits;
+using BankingApp.Transactions.API.Features.PeriodStatement;
+using BankingApp.Transactions.API.Features.Transfers;
+using BankingApp.Transactions.API.Features.Withdraws;
 using BankingApp.Transactions.API.Infrastructure;
+using MediatR.NotificationPublishers;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddMySqlDbContext<AccountsDbContext>(
-    builder.Configuration,
-    builder.Configuration.GetConnectionString(nameof(AccountsDbContext)),
-    ServiceLifetime.Scoped,
-    Assembly.Load("BankingApp.Infrastructure.Core")
-);
-
-builder.Services.AddMediatR(configuration =>
-{
-    configuration.RegisterServicesFromAssemblyContaining<Program>();
-});
-
-builder.Services.AddRabbitMqMessaging(builder.Configuration);
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddMySqlDbContext<AccountsDbContext>(
+        builder.Configuration,
+        builder.Configuration.GetConnectionString(nameof(AccountsDbContext)),
+        ServiceLifetime.Scoped,
+        Assembly.Load("BankingApp.Infrastructure.Core")
+    )
+    .AddMediatR(configuration =>
+    {
+        configuration.RegisterServicesFromAssemblyContaining<Program>();
+        configuration.AddOpenBehavior(typeof(ResilientTransactionBehavior<,>));
+        configuration.NotificationPublisherType = typeof(TaskWhenAllPublisher);
+    })
+    .AddUnitOfWork<AccountsDbContext>()
+    .AddRabbitMqMessaging(builder.Configuration, typeof(Program).Assembly);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.Run();
+app.UseHttpsRedirection();
+
+app.MapPost("/transactions/deposit", DepositEndpoint.PostAsync).WithOpenApi();
+app.MapPost("/transactions/withdraw", WithdrawEndpoint.PostAsync).WithOpenApi();
+app.MapPost("/transactions/transfer", TransferEndpoint.PostAsync).WithOpenApi();
+app.MapGet("/accounts/{token}/statements/period", PeriodStatementEndpoint.GetAsync).WithOpenApi();
+
+await app.Services.ApplyMigrationsAsync<AccountsDbContext>();
+
+await app.RunAsync();
