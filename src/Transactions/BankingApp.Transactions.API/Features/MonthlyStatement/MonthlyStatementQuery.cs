fe7242a1cb5fa4e9048e5bafc25b4866fbfad020
lgcmotta <luiz.motta@warren.com.br>
2023-05-28 19:34:50 -0300

    feat: monthly statement slice

---
+++ src/Transactions/BankingApp.Transactions.API/Features/MonthlyStatement/MonthlyStatementQuery.cs
@@ -0,0 +1,6 @@
+﻿using BankingApp.Domain.Core;
+using MediatR;
+
+namespace BankingApp.Transactions.API.Features.MonthlyStatement;
+
+public record MonthlyStatementQuery(string Token, int Year, int Month) : IRequest<IEnumerable<MonthlyStatementModel>>, IQuery;
