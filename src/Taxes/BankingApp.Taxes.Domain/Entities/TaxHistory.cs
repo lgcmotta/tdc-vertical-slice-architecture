ccba19b2f6699a8bb3d5e9f1de22cbe99ecb7409
lgcmotta <luiz.motta@warren.com.br>
2023-05-29 17:01:48 -0300

    feat: add tax history entity

---
+++ src/Taxes/BankingApp.Taxes.Domain/Entities/TaxHistory.cs
@@ -0,0 +1,20 @@
+﻿using BankingApp.Domain.Core;
+using BankingApp.Taxes.Domain.ValueObjects;
+
+namespace BankingApp.Taxes.Domain.Entities;
+
+public class TaxHistory : IEntity<Guid>, ICreatableEntity
+{
+    public Guid Id { get; set; }
+
+    public Money Amount { get; set; }
+
+    public TaxType Type { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public void SetCreationDateTime(DateTime createdAt)
+    {
+        CreatedAt = createdAt;
+    }
+}
