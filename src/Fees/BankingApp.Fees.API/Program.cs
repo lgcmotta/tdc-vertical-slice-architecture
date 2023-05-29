6cdf28a00318cf88c2ec98103b282747a030a946
lgcmotta <luiz.motta@warren.com.br>
2023-05-29 18:27:24 -0300

    feat: add basic program configuration

--- src/Fees/BankingApp.Fees.API/Program.cs
+++ src/Fees/BankingApp.Fees.API/Program.cs
@@ -1,6 +1,44 @@
+using BankingApp.Application.Core.Behaviors;
+using BankingApp.Application.Core.Extensions;
+using BankingApp.Application.Core.Middlewares;
+using BankingApp.Fees.API.Infrastructure;
+using BankingApp.Fees.API.Infrastructure.Handlers;
+using BankingApp.Infrastructure.Core.Extensions;
+using BankingApp.Infrastructure.Core.Handlers;
+using MediatR.NotificationPublishers;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddEndpointsApiExplorer()
+    .AddSwaggerGen()
+    .AddMySqlDbContext<AccountFeesDbContext>(
+        builder.Configuration,
+        builder.Configuration.GetConnectionString(nameof(AccountFeesDbContext)),
+        ServiceLifetime.Scoped,
+        Assembly.Load("BankingApp.Infrastructure.Core")
+    )
+    .AddMediatR(configuration =>
+    {
+        configuration.RegisterServicesFromAssemblyContaining<Program>();
+        configuration.AddOpenBehavior(typeof(ValidationBehavior<,>));
+        configuration.AddOpenBehavior(typeof(ResilientTransactionBehavior<,>));
+        configuration.NotificationPublisherType = typeof(TaskWhenAllPublisher);
+    })
+    .AddValidators(typeof(Program).Assembly)
+    .AddSingleton<IExceptionHandler, ExceptionHandler>()
+    .AddUnitOfWork<AccountFeesDbContext>()
+    .AddRabbitMqMessaging(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
 
 app.Run();
