9849a769702bd09444236d66aa1bd54385d5721c
lgcmotta <luiz.motta@warren.com.br>
2023-05-29 17:02:04 -0300

    feat: add account representation as an aggregate root

---
+++ src/Taxes/BankingApp.Taxes.Domain/Account.cs
@@ -0,0 +1,16 @@
+﻿using BankingApp.Domain.Core;
+using BankingApp.Taxes.Domain.Entities;
+using BankingApp.Taxes.Domain.ValueObjects;
+
+namespace BankingApp.Taxes.Domain;
+
+public class Account : AggregateRoot<Guid>
+{
+    public new Guid Id { get; set; }
+
+    public Money CurrentBalanceInUSD { get; set; } = Money.Zero;
+
+    public DateTime LastBalanceChange { get; set; }
+
+    public IEnumerable<TaxHistory> TaxHistory { get; set; } = new List<TaxHistory>();
+}
