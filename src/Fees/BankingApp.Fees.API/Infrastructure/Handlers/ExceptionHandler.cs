01cdc8c76cef6db91e0f8b1cb9f90f3dc200b0f0
lgcmotta <luiz.motta@warren.com.br>
2023-05-29 18:27:12 -0300

    feat: add exception handler without implementation

---
+++ src/Fees/BankingApp.Fees.API/Infrastructure/Handlers/ExceptionHandler.cs
@@ -0,0 +1,11 @@
+﻿using BankingApp.Infrastructure.Core.Handlers;
+
+namespace BankingApp.Fees.API.Infrastructure.Handlers;
+
+public class ExceptionHandler : IExceptionHandler
+{
+    public async Task HandleAsync(HttpContext context, Exception exception)
+    {
+        throw new NotImplementedException();
+    }
+}
