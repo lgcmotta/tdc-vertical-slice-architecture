af446915675509cd28fbfcac2f6e3e081332a76f
lgcmotta <luiz.motta@warren.com.br>
2023-05-30 23:32:41 -0300

    feat: implement ExceptionHandler for fees

--- src/Fees/BankingApp.Fees.API/Infrastructure/Handlers/ExceptionHandler.cs
+++ src/Fees/BankingApp.Fees.API/Infrastructure/Handlers/ExceptionHandler.cs
@@ -1,4 +1,7 @@
-﻿using BankingApp.Infrastructure.Core.Handlers;
+﻿using BankingApp.Application.Core.Exceptions;
+using BankingApp.Infrastructure.Core.Handlers;
+using BankingApp.Taxes.Domain.Exceptions;
+using System.Net;
 
 namespace BankingApp.Fees.API.Infrastructure.Handlers;
 
@@ -6,6 +9,21 @@ public class ExceptionHandler : IExceptionHandler
 {
     public async Task HandleAsync(HttpContext context, Exception exception)
     {
-        throw new NotImplementedException();
+        switch (exception)
+        {
+            case AccountNotFoundException
+                or AccountHolderConflictException:
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await context.Response.WriteAsJsonAsync(new { Error = exception.Message });
+                return;
+            case ValidationFailedException validationFailedException:
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                await context.Response.WriteAsJsonAsync(validationFailedException.Errors);
+                return;
+            default:
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                await context.Response.WriteAsJsonAsync(new { Error = "Unexpected error. Please contact the support." });
+                return;
+        }
     }
 }
