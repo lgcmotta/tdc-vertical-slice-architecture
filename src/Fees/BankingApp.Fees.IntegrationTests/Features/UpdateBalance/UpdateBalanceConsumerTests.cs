6a3a5247a4195b1940cf9c6a11797d0b36d212da
lgcmotta <luiz.motta@warren.com.br>
2023-06-01 19:20:38 -0300

    refactor: use rabbitmq for update balance consumer test

--- src/Fees/BankingApp.Fees.IntegrationTests/Features/UpdateBalance/UpdateBalanceConsumerTests.cs
+++ src/Fees/BankingApp.Fees.IntegrationTests/Features/UpdateBalance/UpdateBalanceConsumerTests.cs
@@ -51,26 +51,41 @@ public class UpdateBalanceConsumerTests : IClassFixture<UpdateBalanceConsumerFix
     public async Task UpdateBalanceConsumer_WhenAccountExists_ShouldUpdateBalanceAndLastBalanceChange()
     {
         // Arrange
-        using var scope = _factory.Services.CreateScope();
-        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-        var context = scope.ServiceProvider.GetRequiredService<AccountFeesDbContext>();
+        var harness = _factory.Services.GetTestHarness();
+        using var scope1 = _factory.Services.CreateScope();
+        var context1 = scope1.ServiceProvider.GetRequiredService<AccountFeesDbContext>();
 
         var lastBalanceChange = new DateTime(2023, 05, 31);
         var account = _fixture.CreateAccount(lastBalanceChange: lastBalanceChange);
-        await context.Accounts.AddAsync(account).ConfigureAwait(continueOnCapturedContext: false);
-        await context.SaveChangesAsync().ConfigureAwait(continueOnCapturedContext: false);
+
+        await context1.Accounts.AddAsync(account).ConfigureAwait(continueOnCapturedContext: false);
+        await context1.SaveChangesAsync().ConfigureAwait(continueOnCapturedContext: false);
+
+        await context1.DisposeAsync();
+        scope1.Dispose();
 
         var integrationEvent = _fixture.CreateIntegrationEvent(account.Id);
 
-        _fixture.SetupConsumeContext(integrationEvent);
 
-        var consumer = new UpdateBalanceConsumer(mediator);
+        var consumerHarness = harness.GetConsumerHarness<UpdateBalanceConsumer>();
 
         // Act
-        await consumer.Consume(_fixture.ConsumeContext);
+        await harness.Bus.Publish(integrationEvent, CancellationToken.None).ConfigureAwait(continueOnCapturedContext: false);
+
+        await Task.Delay(5000).ConfigureAwait(continueOnCapturedContext: false);
 
         // Assert
-        account.CurrentBalanceInUSD.Should().Be(new Money(integrationEvent.Balance));
-        account.LastBalanceChange.Should().BeAfter(lastBalanceChange);
+        var consumed = await consumerHarness.Consumed.Any().ConfigureAwait(continueOnCapturedContext: false);
+
+        using var scope2 = _factory.Services.CreateScope();
+        var context2 = scope2.ServiceProvider.GetRequiredService<AccountFeesDbContext>();
+
+        var updatedAccount = await context2.Accounts
+            .FirstOrDefaultAsync(updated => updated.Id == account.Id)
+            .ConfigureAwait(continueOnCapturedContext: false);
+
+        consumed.Should().BeTrue();
+        updatedAccount!.CurrentBalanceInUSD.Should().Be(new Money(integrationEvent.Balance));
+        updatedAccount.LastBalanceChange.Should().BeAfter(lastBalanceChange);
     }
 }
