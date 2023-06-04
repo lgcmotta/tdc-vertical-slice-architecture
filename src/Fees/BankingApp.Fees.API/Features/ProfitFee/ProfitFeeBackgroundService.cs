1eaf5fdb672841c87e49053eac1cf6d7f1280c31
lgcmotta <luiz.motta@warren.com.br>
2023-06-04 15:54:06 -0300

    feat: add activity source to background services

--- src/Fees/BankingApp.Fees.API/Features/ProfitFee/ProfitFeeBackgroundService.cs
+++ src/Fees/BankingApp.Fees.API/Features/ProfitFee/ProfitFeeBackgroundService.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.Extensions.Options;
+using System.Diagnostics;
 
 namespace BankingApp.Fees.API.Features.ProfitFee;
 
@@ -8,12 +9,14 @@ public class ProfitFeeBackgroundService : BackgroundService
     private readonly IServiceProvider _serviceProvider;
     private readonly IOptions<ProfitFeeOptions> _options;
     private readonly PeriodicTimer _timer;
+    private readonly ActivitySource _activitySource;
 
     public ProfitFeeBackgroundService(IServiceProvider serviceProvider, IOptions<ProfitFeeOptions> options)
     {
         _serviceProvider = serviceProvider;
         _options = options;
         _timer = new PeriodicTimer(options.Value.ExecutionInterval);
+        _activitySource = new ActivitySource(nameof(ProfitFeeBackgroundService));
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -23,6 +26,8 @@ public class ProfitFeeBackgroundService : BackgroundService
             using var scope = _serviceProvider.CreateScope();
 
             var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+            using var activity = _activitySource.StartActivity(nameof(ExecuteAsync), ActivityKind.Server);
 
             var command = new ProfitFeeCommand(_options.Value.Rate, _options.Value.BalanceIdleInMinutes);
 
