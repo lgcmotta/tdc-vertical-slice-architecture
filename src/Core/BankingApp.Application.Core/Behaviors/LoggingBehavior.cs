33355414d655a674c311d84e5b4888716dbd0c3b
lgcmotta <luiz.motta@warren.com.br>
2023-06-08 21:36:12 -0300

    feat: add logging behavior

---
+++ src/Core/BankingApp.Application.Core/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,45 @@
+﻿using BankingApp.Infrastructure.Core.Extensions;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace BankingApp.Application.Core.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull
+{
+    private readonly ILogger _logger;
+
+    public LoggingBehavior(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public LoggingBehavior(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    {
+        _logger = logger;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var behaviorName = typeof(LoggingBehavior<TRequest, TResponse>).GetGenericTypeName();
+
+        var requestType = typeof(TRequest);
+
+        try
+        {
+            _logger.LogInformation("[{Behavior}] - Handling request of type {RequestType}", behaviorName, requestType);
+
+            var response = await next().ConfigureAwait(continueOnCapturedContext: false);
+
+            _logger.LogInformation("[{Behavior}] - Request of type {RequestType} handled successfully", behaviorName, requestType);
+
+            return response;
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception, "[{Behavior}] - An exception occurred while handling request of type {RequestType}", behaviorName, requestType);
+
+            throw;
+        }
+    }
+}
