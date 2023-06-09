13869bb12998d2c1f8f90da9dbd033e3b3f99e48
lgcmotta <luiz.motta@warren.com.br>
2023-06-08 21:36:01 -0300

    feat: add logging support on infrastructure core project

---
+++ src/Core/BankingApp.Infrastructure.Core/Extensions/InfrastructureLoggingBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using Serilog;
+
+namespace BankingApp.Infrastructure.Core.Extensions;
+
+public static class InfrastructureLoggingBuilderExtensions
+{
+    public static ILoggingBuilder ConfigureSerilogForOpenTelemetry(this ILoggingBuilder builder)
+    {
+        builder.ClearProviders();
+
+        var logger = new LoggerConfiguration()
+            .WriteTo.OpenTelemetry()
+            .CreateLogger();
+
+        Log.Logger = logger;
+
+        builder.AddSerilog(logger);
+
+        return builder;
+    }
+}
---
+++ src/Core/BankingApp.Infrastructure.Core/Extensions/InfrastructureTypeExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace BankingApp.Infrastructure.Core.Extensions;
+
+public static class InfrastructureTypeExtensions
+{
+    public static string GetGenericTypeName(this Type type)
+    {
+        if (!type.IsGenericType) return type.Name;
+
+        var genericTypes = string.Join(",", type.GetGenericArguments().Select(argument => argument.Name));
+
+        var genericTypeName = $"{type.Name.Replace("`", string.Empty)}<{genericTypes}>";
+
+        return genericTypeName;
+    }
+}
